b4dfbd252acbe58204b8a0b0af4ef9e271034d5f
Amico <Amico>
2023-01-30 15:58:03 +0100

    delete ref file pn-ssfile.xml

--- src/main/java/it/pagopa/pnss/transformation/service/CommonArubaService.java
+++ src/main/java/it/pagopa/pnss/transformation/service/CommonArubaService.java
@@ -63,11 +63,11 @@ public abstract class CommonArubaService {
         JAXBContext jaxbContext = JAXBContext.newInstance(SignRequestV2.class);
         Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        File file = new File("C:\\PROGETTI\\DGSPA\\workspace\\pn-ssfile.xml");
+        //File file = new File("C:\\PROGETTI\\DGSPA\\workspace\\pn-ssfile.xml");
         JAXBElement <SignRequestV2> jaxbElement
                 = new JAXBElement <SignRequestV2>(new QName("", "SignRequest"), SignRequestV2.class, signRequestV2);
         jaxbMarshaller.marshal(jaxbElement, System.out);
-        jaxbMarshaller.marshal(jaxbElement, file);
+        //jaxbMarshaller.marshal(jaxbElement, file);
     }
 
     public ArubaSignServiceService getArubaSignService() {

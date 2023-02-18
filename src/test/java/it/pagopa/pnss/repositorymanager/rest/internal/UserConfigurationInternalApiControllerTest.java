e23ee42f8550de45e58bfa838edd1aa360ecdba6
mottone-dgs <119047198+mottone-dgs@users.noreply.github.com>
2023-02-18 09:39:18 +0100

    Revert "aggiunto campo canModifyStatus + allineamento reactor"

--- src/test/java/it/pagopa/pnss/repositorymanager/rest/internal/UserConfigurationInternalApiControllerTest.java
+++ src/test/java/it/pagopa/pnss/repositorymanager/rest/internal/UserConfigurationInternalApiControllerTest.java
@@ -73,8 +73,6 @@ public class UserConfigurationInternalApiControllerTest {
 		canCreate.add("A");
 		List<String> canRead  = new ArrayList<>();
 		canRead.add("DD");
-		List<String> canModifyStatus = new ArrayList<>();
-		canModifyStatus.add("ModifyStatus");
 		UserConfigurationDestination destination = new UserConfigurationDestination(); 
 		destination.setSqsUrl("URL");
 		
@@ -82,7 +80,6 @@ public class UserConfigurationInternalApiControllerTest {
 		userConfigurationInput.setName(PARTITION_ID_ENTITY);
 		userConfigurationInput.setCanCreate(canCreate);
 		userConfigurationInput.setCanRead(canRead);
-		userConfigurationInput.setCanModifyStatus(canModifyStatus);
 		userConfigurationInput.setSignatureInfo("mmm");
 		userConfigurationInput.setDestination(destination);
 		userConfigurationInput.setApiKey("apiKey");
@@ -91,13 +88,10 @@ public class UserConfigurationInternalApiControllerTest {
 		canCreate1.add("A");
 		List<String> canRead1  = new ArrayList<>();
 		canRead1.add("DD");
-		List<String> canModifyStatus1  = new ArrayList<>();
-		canModifyStatus1.add("ModifyStatus");
 		
 		userConfigurationChanges = new UserConfigurationChanges();
 		userConfigurationChanges.setCanCreate(canCreate1);
 		userConfigurationChanges.setCanRead(canRead1);
-		userConfigurationChanges.setCanModifyStatus(canModifyStatus1);
 	}
 	
 	@Test
@@ -149,7 +143,7 @@ public class UserConfigurationInternalApiControllerTest {
 			        .contentType(APPLICATION_JSON)
 			        .body(BodyInserters.fromValue(userConfigurationInput))
 			        .exchange()
-			        .expectStatus().isEqualTo(HttpStatus.CONFLICT);
+			        .expectStatus().isEqualTo(HttpStatus.FORBIDDEN);
 		
 		}
 		

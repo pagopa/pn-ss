35e0561ff242fe63fe32746295348a937f3be17b
s.delluomo <s.delluomo@posta.it>
2023-02-28 19:25:49 +0100

    passati headers sicurezza per ConfigurationApiCall

--- src/main/java/it/pagopa/pnss/common/retention/RetentionService.java
+++ src/main/java/it/pagopa/pnss/common/retention/RetentionService.java
@@ -10,9 +10,13 @@ public interface RetentionService {
 	
 //	Mono<PutObjectLockConfigurationRequest> getPutObjectLockConfigurationRequest(String documentKey, String documentState, DocumentType documentType) throws RetentionException;
 	
-	Mono<PutObjectRequest> getPutObjectRequestForObjectInBucket(String bucketName, byte[] contentBytes, String documentKey, String documentState, String documentType) throws RetentionException;
-	
-	Mono<PutObjectRequest> getPutObjectRequestForPresignRequest(String bucketName, String keyName, String contenType, Map<String,String> secret, 
+	Mono<PutObjectRequest> getPutObjectRequestForObjectInBucket(
+			String bucketName, byte[] contentBytes, 
 			String documentKey, String documentState, String documentType) throws RetentionException;
+	
+	Mono<PutObjectRequest> getPutObjectRequestForPresignRequest(
+			String bucketName, String documentKey, String contenType, Map<String,String> secret, 
+			String documentState, String documentType,
+			String authPagopaSafestorageCxId, String authApiKey) throws RetentionException;
 
 }

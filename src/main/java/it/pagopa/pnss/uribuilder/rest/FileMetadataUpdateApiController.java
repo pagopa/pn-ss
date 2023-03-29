52ca9314cf14fa705f4626db4032e7a3f0cffe24
Leonardo Marziali <leonardo.marziali@dgsspa.com>
2023-03-29 17:38:24 +0200

    Update FileMetadataUpdateApiController

--- src/main/java/it/pagopa/pnss/uribuilder/rest/FileMetadataUpdateApiController.java
+++ src/main/java/it/pagopa/pnss/uribuilder/rest/FileMetadataUpdateApiController.java
@@ -1,41 +1,42 @@
 package it.pagopa.pnss.uribuilder.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
-
 import it.pagopa.pn.template.rest.v1.api.FileMetadataUpdateApi;
 import it.pagopa.pn.template.rest.v1.dto.OperationResultCodeResponse;
 import it.pagopa.pn.template.rest.v1.dto.UpdateFileMetadataRequest;
 import it.pagopa.pnss.uribuilder.service.FileMetadataUpdateService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequiredArgsConstructor
 public class FileMetadataUpdateApiController implements FileMetadataUpdateApi {
-	
+
+    final FileMetadataUpdateService service;
+
     @Value("${header.x-api-key}")
     private String apiKey;
+
     @Value("${header.x-pagopa-safestorage-cx-id}")
-    private String pagopaSafestorageCxId;	
+    private String pagopaSafestorageCxId;
 
-    @Autowired
-    FileMetadataUpdateService service;
-
+    public FileMetadataUpdateApiController(FileMetadataUpdateService service) {
+        this.service = service;
+    }
 
     @Override
-    public Mono<ResponseEntity<OperationResultCodeResponse>> updateFileMetadata
-            (String fileKey, String xPagopaSafestorageCxId, Mono<UpdateFileMetadataRequest> updateFileMetadataRequest, final ServerWebExchange exchange) {
+    public Mono<ResponseEntity<OperationResultCodeResponse>> updateFileMetadata(String fileKey, String xPagopaSafestorageCxId,
+                                                                                Mono<UpdateFileMetadataRequest> updateFileMetadataRequest
+            , final ServerWebExchange exchange) {
 
-    	String pagopaSafestorageCxIdValue = exchange.getRequest().getHeaders().getFirst(pagopaSafestorageCxId);
-    	String apiKeyValue = exchange.getRequest().getHeaders().getFirst(apiKey);
-    	
-        return updateFileMetadataRequest.flatMap(request -> service.createUriForUploadFile(
-        																fileKey,xPagopaSafestorageCxId, request,
-        																pagopaSafestorageCxIdValue, apiKeyValue))
-                .map(ResponseEntity::ok);
+        String pagopaSafestorageCxIdValue = exchange.getRequest().getHeaders().getFirst(pagopaSafestorageCxId);
+        String apiKeyValue = exchange.getRequest().getHeaders().getFirst(apiKey);
+
+        return updateFileMetadataRequest.flatMap(request -> service.createUriForUploadFile(fileKey,
+                                                                                           xPagopaSafestorageCxId,
+                                                                                           request,
+                                                                                           pagopaSafestorageCxIdValue,
+                                                                                           apiKeyValue)).map(ResponseEntity::ok);
     }
 }

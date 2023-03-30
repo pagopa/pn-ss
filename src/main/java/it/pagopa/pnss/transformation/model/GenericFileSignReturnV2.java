f882e2fcf4783bbdf013ec9751dc792418f46393
michelescara <michele.scaramuzzino@gmail.com>
2023-03-30 10:21:59 +0200

    Generated equals and hashCode for GenericFileSignReturnV2 and PdfFileSignReturnV2

--- src/main/java/it/pagopa/pnss/transformation/model/GenericFileSignReturnV2.java
+++ src/main/java/it/pagopa/pnss/transformation/model/GenericFileSignReturnV2.java
@@ -2,6 +2,8 @@ package it.pagopa.pnss.transformation.model;
 
 import it.pagopa.pnss.transformation.wsdl.SignReturnV2;
 
+import java.util.Objects;
+
 public class GenericFileSignReturnV2 extends ErrorResponse {
     SignReturnV2 pdfInfoResultSign;
 
@@ -11,5 +13,19 @@ public class GenericFileSignReturnV2 extends ErrorResponse {
 
     public void setPdfInfoResultSign(SignReturnV2 pdfInfoResultSign) {
         this.pdfInfoResultSign = pdfInfoResultSign;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        GenericFileSignReturnV2 that = (GenericFileSignReturnV2) o;
+        return Objects.equals(pdfInfoResultSign, that.pdfInfoResultSign);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), pdfInfoResultSign);
     }
 }
--- src/main/java/it/pagopa/pnss/transformation/model/PdfFileSignReturnV2.java
+++ src/main/java/it/pagopa/pnss/transformation/model/PdfFileSignReturnV2.java
@@ -2,7 +2,9 @@ package it.pagopa.pnss.transformation.model;
 
 import it.pagopa.pnss.transformation.wsdl.SignReturnV2;
 
-public class PdfFileSignReturnV2 extends ErrorResponse{
+import java.util.Objects;
+
+public class PdfFileSignReturnV2 extends ErrorResponse {
     SignReturnV2 pdfInfoResultSign;
 
     public SignReturnV2 getPdfInfoResultSign() {
@@ -12,4 +14,19 @@ public class PdfFileSignReturnV2 extends ErrorResponse{
     public void setPdfInfoResultSign(SignReturnV2 pdfInfoResultSign) {
         this.pdfInfoResultSign = pdfInfoResultSign;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PdfFileSignReturnV2 that = (PdfFileSignReturnV2) o;
+        return Objects.equals(pdfInfoResultSign, that.pdfInfoResultSign);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), pdfInfoResultSign);
+    }
+
 }

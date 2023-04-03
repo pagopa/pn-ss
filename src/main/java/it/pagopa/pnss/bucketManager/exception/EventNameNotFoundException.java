c5063d7f2eced137d042c712e147e7d8ee9182d6
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-04-03 16:31:06 +0200

    Remove sdk lambda dependencies, fix UriBuilderUploadTest assertions

--- src/main/java/it/pagopa/pnss/bucketManager/exception/EventNameNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.pnss.bucketManager.exception;
-
-public class EventNameNotFoundException extends RuntimeException{
-    public EventNameNotFoundException() {
-        super(("The Event name record is not correct"));
-    }
-}
-
-

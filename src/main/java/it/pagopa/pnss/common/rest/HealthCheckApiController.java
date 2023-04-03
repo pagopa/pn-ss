0f8327f6257fa28c64f5f5eb7967369ac9570559
FedericoMagini <federico.magini@gmail.com>
2023-04-03 17:21:02 +0200

    fix issue in ASWConfiguration

--- src/main/java/it/pagopa/pnss/common/rest/HealthCheckApiController.java
+++ src/main/java/it/pagopa/pnss/common/rest/HealthCheckApiController.java
@@ -3,14 +3,13 @@ package it.pagopa.pnss.common.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class HealthCheckApiController {
 
 	@GetMapping(value = "/")
-    public Mono<ResponseEntity<Void>> status(ServerWebExchange exchange) {
+    public Mono<ResponseEntity<Void>> status() {
         return Mono.just(ResponseEntity.ok().build());
     }
 }

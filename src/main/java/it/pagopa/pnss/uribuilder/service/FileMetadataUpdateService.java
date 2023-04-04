c09da86cff8d978a5e871ce20ab3b6f3afd2ff57
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-04-04 17:50:55 +0200

    Change service name

--- src/main/java/it/pagopa/pnss/uribuilder/service/FileMetadataUpdateService.java
+++ src/main/java/it/pagopa/pnss/uribuilder/service/FileMetadataUpdateService.java
@@ -23,116 +23,120 @@ import java.text.SimpleDateFormat;
 @Slf4j
 public class FileMetadataUpdateService {
 
-	private final UserConfigurationClientCall userConfigClientCall;
-	private final DocumentClientCall docClientCall;
-	private final DocTypesClientCall docTypesClientCall;
+    private final UserConfigurationClientCall userConfigClientCall;
+    private final DocumentClientCall docClientCall;
+    private final DocTypesClientCall docTypesClientCall;
 
-	public FileMetadataUpdateService(UserConfigurationClientCall userConfigurationClientCall, DocumentClientCall documentClientCall, DocTypesClientCall docTypesClientCall) {
-		this.userConfigClientCall = userConfigurationClientCall;
-		this.docClientCall = documentClientCall;
-		this.docTypesClientCall = docTypesClientCall;
-	}
+    public FileMetadataUpdateService(UserConfigurationClientCall userConfigurationClientCall, DocumentClientCall documentClientCall,
+                                     DocTypesClientCall docTypesClientCall) {
+        this.userConfigClientCall = userConfigurationClientCall;
+        this.docClientCall = documentClientCall;
+        this.docTypesClientCall = docTypesClientCall;
+    }
 
-	public Mono<OperationResultCodeResponse> createUriForUploadFile(String fileKey, String xPagopaSafestorageCxId, UpdateFileMetadataRequest request, String authPagopaSafestorageCxId, String authApiKey) {
-		var retentionUntil = request.getRetentionUntil();
-		var logicalState = request.getStatus();
+    public Mono<OperationResultCodeResponse> updateMetadata(String fileKey, String xPagopaSafestorageCxId,
+                                                            UpdateFileMetadataRequest request, String authPagopaSafestorageCxId,
+                                                            String authApiKey) {
+        var retentionUntil = request.getRetentionUntil();
+        var logicalState = request.getStatus();
 
-		return docClientCall.getDocument(fileKey)
+        return docClientCall.getDocument(fileKey)
 
-				   .flatMap(documentResponse -> {
-					   var document = documentResponse.getDocument();
-					   var documentType = document.getDocumentType().getTipoDocumento();
+                            .flatMap(documentResponse -> {
+                                var document = documentResponse.getDocument();
+                                var documentType = document.getDocumentType().getTipoDocumento();
 
-					   Mono<String> checkedStatus;
-					   if (logicalState != null && !logicalState.isBlank()) {
-						   checkedStatus = checkLookUp(documentType, logicalState);
-					   } else {
-						   checkedStatus = Mono.just("");
-					   }
-					   return Mono.zip(userConfigClientCall.getUser(xPagopaSafestorageCxId), checkedStatus)
-							   .map(objects -> Tuples.of(document, objects.getT1(), objects.getT2()));
-				   })
-				   .flatMap(objects -> {
-					   var document = objects.getT1();
-					   var documentType = document.getDocumentType();
-					   var tipoDocumento = documentType.getTipoDocumento();
-					   var userConfiguration = objects.getT2().getUserConfiguration();
-					   var technicalStatus = objects.getT3();
-					   var documentChanges = new DocumentChanges();
+                                Mono<String> checkedStatus;
+                                if (logicalState != null && !logicalState.isBlank()) {
+                                    checkedStatus = checkLookUp(documentType, logicalState);
+                                } else {
+                                    checkedStatus = Mono.just("");
+                                }
+                                return Mono.zip(userConfigClientCall.getUser(xPagopaSafestorageCxId), checkedStatus)
+                                           .map(objects -> Tuples.of(document, objects.getT1(), objects.getT2()));
+                            }).flatMap(objects -> {
+                    var document = objects.getT1();
+                    var documentType = document.getDocumentType();
+                    var tipoDocumento = documentType.getTipoDocumento();
+                    var userConfiguration = objects.getT2().getUserConfiguration();
+                    var technicalStatus = objects.getT3();
+                    var documentChanges = new DocumentChanges();
 
-					   if (userConfiguration.getCanModifyStatus() == null || !userConfiguration.getCanModifyStatus().contains(tipoDocumento)) {
-						   String errore = String.format("Client '%s' not has privilege for change document " + "type '%s'",
-														 xPagopaSafestorageCxId,
-														 tipoDocumento);
-						   log.error("FileMetadataUpdateService.createUriForUploadFile() : errore = {}", errore);
-						   return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, errore));
-					   }
+                    if (userConfiguration.getCanModifyStatus() == null || !userConfiguration.getCanModifyStatus().contains(tipoDocumento)) {
+                        String errore = String.format("Client '%s' not has privilege for change document " + "type '%s'",
+                                                      xPagopaSafestorageCxId,
+                                                      tipoDocumento);
+                        log.error("FileMetadataUpdateService.createUriForUploadFile() : errore = {}", errore);
+                        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, errore));
+                    }
 
-					   boolean isStatusPresent = false;
+                    boolean isStatusPresent = false;
 
-					   if (request.getStatus() != null && !StringUtils.isBlank(request.getStatus())) {
-						   log.info("FileMetadataUpdateService.createUriForUploadFile() : request.getStatus() isn't blank or null:" + " CONTINUO");
-						   if (documentType.getStatuses() != null) {
-							   isStatusPresent = documentType.getStatuses().containsKey(logicalState);
-						   }
-						   if (!isStatusPresent) {
-							   log.error("FileMetadataUpdateService.createUriForUploadFile() : Status not found for document" + " key {}",
-									   fileKey);
-							   return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST,
-									   "Status not found for document key : " + fileKey));
-						   }
+                    if (request.getStatus() != null && !StringUtils.isBlank(request.getStatus())) {
+                        if (documentType.getStatuses() != null) {
+                            isStatusPresent = documentType.getStatuses().containsKey(logicalState);
+                        }
+                        if (!isStatusPresent) {
+                            log.error("FileMetadataUpdateService.createUriForUploadFile() : Status '{}' not found for document" + " key {}",
+                                      request.getStatus(),
+                                      fileKey);
+                            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                                                                          "Status not found for document key : " + fileKey));
+                        }
 
-						   if (StringUtils.isEmpty(technicalStatus)) {
-							   log.error("FileMetadataUpdateService.createUriForUploadFile() : Technical status not found " +
-									   "for document key {}", fileKey);
-							   return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST,
-									   "Technical status not found for document key : " + fileKey));
-						   }
+                        if (StringUtils.isEmpty(technicalStatus)) {
+                            log.error("FileMetadataUpdateService.createUriForUploadFile() : Technical status not found " +
+                                      "for document key {}", fileKey);
+                            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                                                                          "Technical status not found for document key : " + fileKey));
+                        }
 
-					   }
-						   documentChanges.setDocumentState(technicalStatus);
+                    }
+                    documentChanges.setDocumentState(technicalStatus);
 
-					   if (retentionUntil != null) {
-						   documentChanges.setRetentionUntil(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(retentionUntil));
-					   }
+                    if (retentionUntil != null) {
+                        documentChanges.setRetentionUntil(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").format(retentionUntil));
+                    }
 
-					   return docClientCall.patchDocument(authPagopaSafestorageCxId, authApiKey, fileKey, documentChanges).flatMap(documentResponsePatch -> {
-						   OperationResultCodeResponse resp = new OperationResultCodeResponse();
-						   resp.setResultCode(ResultCodeWithDescription.OK.getResultCode());
-						   resp.setResultDescription(ResultCodeWithDescription.OK.getDescription());
-						   return Mono.just(resp);
-					   });
+                    return docClientCall.patchDocument(authPagopaSafestorageCxId, authApiKey, fileKey, documentChanges)
+                                        .flatMap(documentResponsePatch -> {
+                                            OperationResultCodeResponse resp = new OperationResultCodeResponse();
+                                            resp.setResultCode(ResultCodeWithDescription.OK.getResultCode());
+                                            resp.setResultDescription(ResultCodeWithDescription.OK.getDescription());
+                                            return Mono.just(resp);
+                                        });
 
-				   })
+                })
 
-				   .onErrorResume(WebClientResponseException.class, e -> {
-					   log.error(
-							   "FileMetadataUpdateService.createUriForUploadFile() : rilevata una WebClientResponseException : errore = {}",
-							   e.getMessage(),
-							   e);
-					   return Mono.error(new ResponseStatusException(e.getStatusCode(), e.getMessage()));
-				   })
+                            .onErrorResume(WebClientResponseException.class, e -> {
+                                log.error(
+                                        "FileMetadataUpdateService.createUriForUploadFile() : rilevata una WebClientResponseException : " +
+                                        "errore = {}", e.getMessage(), e);
+                                return Mono.error(new ResponseStatusException(e.getStatusCode(), e.getMessage()));
+                            })
 
-				   .onErrorResume(DocumentKeyNotPresentException.class, e -> {
-					   log.error("FileMetadataUpdateService.createUriForUploadFile() : rilevata una DocumentKeyNotPresentException : errore" +
-								 " = " + "{}", e.getMessage(), e);
-					   return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage()));
-				   })
+                            .onErrorResume(DocumentKeyNotPresentException.class, e -> {
+                                log.error(
+                                        "FileMetadataUpdateService.createUriForUploadFile() : rilevata una DocumentKeyNotPresentException" +
+                                        " : errore" + " = " + "{}", e.getMessage(), e);
+                                return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage()));
+                            })
 
-				   .onErrorResume(e -> {
-					   log.error("FileMetadataUpdateService.createUriForUploadFile() : errore generico = {}", e.getMessage(), e);
-					   return Mono.error(e);
-				   });
-	}
+                            .onErrorResume(e -> {
+                                log.error("FileMetadataUpdateService.createUriForUploadFile() : errore generico = {}", e.getMessage(), e);
+                                return Mono.error(e);
+                            });
+    }
 
     private Mono<String> checkLookUp(String documentType, String logicalState) {
         return docTypesClientCall.getdocTypes(documentType)//
-                .map(item -> item.getDocType().getStatuses().get(logicalState).getTechnicalState())//
-                .onErrorResume(NullPointerException.class, e -> {
-					String errorMsg = String.format("Status %s is not valid for DocumentType %s", logicalState, documentType);
-					log.error("NullPointerException: {}", errorMsg);
-					return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMsg));
-                });
+                                 .map(item -> item.getDocType().getStatuses().get(logicalState).getTechnicalState())//
+                                 .onErrorResume(NullPointerException.class, e -> {
+                                     String errorMsg =
+                                             String.format("Status %s is not valid for DocumentType %s", logicalState, documentType);
+                                     log.error("NullPointerException: {}", errorMsg);
+                                     return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMsg));
+                                 });
     }
 
 }

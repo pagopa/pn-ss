d3d666010aea2c76e0ace695764d714f995f5f54
michelescara <michele.scaramuzzino@gmail.com>
2023-04-05 11:53:49 +0200

    Revised retention period parsing logic. Added test cases for "getRetentionPeriodInDays" method.

---
+++ src/test/java/it/pagopa/pnss/retention/RetentionServiceTest.java
@@ -0,0 +1,40 @@
+package it.pagopa.pnss.retention;
+
+import it.pagopa.pnss.common.client.exception.RetentionException;
+import it.pagopa.pnss.common.retention.RetentionService;
+import it.pagopa.pnss.common.retention.RetentionServiceImpl;
+import it.pagopa.pnss.testutils.annotation.SpringBootTestWebEnv;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+
+import java.lang.annotation.Retention;
+
+@SpringBootTestWebEnv
+@AutoConfigureWebTestClient
+public class RetentionServiceTest {
+
+    @Autowired
+    private RetentionServiceImpl retentionService;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"10y", "5Y", "10d", "5D", "1Y 1d", "10y 5d", "1y 10D", "10Y 10D"})
+    void getRetentionPeriodInDaysOk(String retentionPeriod) {
+        Assertions.assertNotNull(retentionService.getRetentionPeriodInDays(retentionPeriod));
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    @ValueSource(strings = {" ", "d"})
+    void getRetentionPeriodInDaysKo(String retentionPeriod) {
+        Assertions.assertThrows(RetentionException.class, () -> retentionService.getRetentionPeriodInDays(retentionPeriod));
+    }
+
+}

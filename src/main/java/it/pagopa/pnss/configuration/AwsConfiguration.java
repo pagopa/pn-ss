bdf4ba99da318f048c3651710a8fd7c8b67951a3
michelescara <michele.scaramuzzino@gmail.com>
2023-05-08 17:52:10 +0200

    Decommented "executor.execute(worker)" line.

--- src/main/java/it/pagopa/pnss/configuration/AwsConfiguration.java
+++ src/main/java/it/pagopa/pnss/configuration/AwsConfiguration.java
@@ -288,7 +288,7 @@ public class AwsConfiguration {
                                                                              amazonDynamoDB,
                                                                              cloudWatchClient);
             if (testEventBridge == null) {
-                //executor.execute(worker);
+                executor.execute(worker);
             }
         };
     }

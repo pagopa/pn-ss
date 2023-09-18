2eef42aee3aa24e7e8b17a13373c559d66d305c2
DragosConovici <conovicidragos00@gmail.com>
2023-09-18 16:14:01 +0200

    Added more tests for ArubaSignServiceCallImpl class

--- src/test/java/it/pagopa/pnss/transformation/rest/call/aruba/ArubaSignServiceCallImplTest.java
+++ src/test/java/it/pagopa/pnss/transformation/rest/call/aruba/ArubaSignServiceCallImplTest.java
@@ -1,22 +1,10 @@
 package it.pagopa.pnss.transformation.rest.call.aruba;
 
+import it.pagopa.pnss.common.client.exception.ArubaSignException;
 import it.pagopa.pnss.testutils.annotation.SpringBootTestWebEnv;
-import it.pagopa.pnss.transformation.wsdl.ArubaSignService;
-import it.pagopa.pnss.transformation.wsdl.PdfsignatureV2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.testcontainers.shaded.org.apache.commons.lang3.concurrent.ConcurrentUtils;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-import software.amazon.awssdk.core.ResponseBytes;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
-
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @SpringBootTestWebEnv
 public class ArubaSignServiceCallImplTest {
@@ -24,21 +12,35 @@ public class ArubaSignServiceCallImplTest {
     @Autowired
     ArubaSignServiceCallImpl arubaSignServiceCall;
 
-    @Autowired
-    ArubaSignService arubaSignService;
+    private static final byte[] byteFile = "Stringa di prova".getBytes();
+    private static final boolean marcatura = true;
+
+    //Controllare come mockare ArubaSignService ritornando un Future per scrivere i casi di test Ok
+    @Test
+    void signPdfDocumentKo(){
+
+        var testMono = arubaSignServiceCall.signPdfDocument(byteFile, marcatura);
+
+        StepVerifier.create(testMono).expectError(ArubaSignException.class).verify();
+    }
 
     @Test
-    void signPdfDocumentOk(){
+    void pkcs7signV2Ko(){
         byte[] pdfFile = "Stringa di prova".getBytes();
         boolean marcatura = true;
 
-        Future<?> future = ConcurrentUtils.constantFuture(T myValue);
-        Future<?> future = future.get(1, TimeUnit.);
+        var testMono = arubaSignServiceCall.pkcs7signV2(byteFile, marcatura);
 
-        when(arubaSignService.pdfsignatureV2Async(any(PdfsignatureV2.class), any())).thenReturn(Mono.just(future));
+        StepVerifier.create(testMono).expectError(ArubaSignException.class).verify();
+    }
 
-        var testMono = arubaSignServiceCall.signPdfDocument(pdfFile, marcatura);
+    @Test
+    void xmlSignatureKo(){
+        byte[] pdfFile = "Stringa di prova".getBytes();
+        boolean marcatura = true;
 
-        StepVerifier.create(testMono).expectNextCount(1).verifyComplete();
+        var testMono = arubaSignServiceCall.xmlSignature(byteFile, marcatura);
+
+        StepVerifier.create(testMono).expectError(ArubaSignException.class).verify();
     }
 }

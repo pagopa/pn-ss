7067f55ad47d01a3b2cd7d863906abcac5cde7d9
michelescara <michelescaramuzzino@gmail.com>
2023-11-15 17:07:21 +0100

    Fixed test failure.

--- src/test/java/it/pagopa/pnss/localstack/LocalStackClientConfig.java
+++ src/test/java/it/pagopa/pnss/localstack/LocalStackClientConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.sqs.SqsClient;
 
 import java.net.URI;
 
@@ -18,14 +17,14 @@ public class LocalStackClientConfig {
     @Autowired
     private AwsConfigurationProperties awsConfigurationProperties;
     @Value("${test.aws.s3.endpoint}")
-    String sqsLocalStackEndpoint;
+    String s3LocalStackEndpoint;
 
     @Bean
     public S3Client s3TestClient() {
         return S3Client.builder()
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .region(Region.of(awsConfigurationProperties.regionCode()))
-                .endpointOverride(URI.create(sqsLocalStackEndpoint))
+                .endpointOverride(URI.create(s3LocalStackEndpoint))
                 .build();
     }
 

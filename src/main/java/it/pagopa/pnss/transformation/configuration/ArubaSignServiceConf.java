e6a4404872933eb70cc09c9a58df2b5d3d3f815f
damiano.lozzi <damiano.lozzi@dgsspa.com>
2023-11-22 15:08:36 +0100

    implemented proxy client for Aruba

--- src/main/java/it/pagopa/pnss/transformation/configuration/ArubaSignServiceConf.java
+++ src/main/java/it/pagopa/pnss/transformation/configuration/ArubaSignServiceConf.java
@@ -2,17 +2,25 @@ package it.pagopa.pnss.transformation.configuration;
 
 import it.pagopa.pnss.transformation.wsdl.ArubaSignService;
 import it.pagopa.pnss.transformation.wsdl.ArubaSignServiceService;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
 
 @Configuration
 public class ArubaSignServiceConf {
-
+    @Value("${aruba.server.address}")
+    private String arubaServerAddress;
     @Bean
-    public ArubaSignService arubaSignService() throws IOException {
-        return new ArubaSignServiceService(new ClassPathResource("wsdl/ArubaSignService.wsdl").getURL()).getArubaSignServicePort();
-    }
+    public ArubaSignService arubaSignService() {
+
+            JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+            factory.setServiceClass(ArubaSignService.class);
+            factory.setAddress(arubaServerAddress);
+            factory.setWsdlLocation(ArubaSignServiceService.WSDL_LOCATION.getPath());
+            factory.setEndpointName(ArubaSignServiceService.ArubaSignServicePort);
+            factory.setServiceName(ArubaSignServiceService.SERVICE);
+            return factory.create(ArubaSignService.class);
+        }
 }

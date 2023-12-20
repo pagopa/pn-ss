67a020f7fa59fa92d31173c418effb9c55bd130a
michelescara <michelescaramuzzino@gmail.com>
2023-12-20 17:56:41 +0100

    Removed setWsdlLocation from pn-ss aruba proxy client initialization.
    Changed aruba.server.address default value.

--- src/main/java/it/pagopa/pnss/transformation/configuration/ArubaSignServiceConf.java
+++ src/main/java/it/pagopa/pnss/transformation/configuration/ArubaSignServiceConf.java
@@ -2,25 +2,30 @@ package it.pagopa.pnss.transformation.configuration;
 
 import it.pagopa.pnss.transformation.wsdl.ArubaSignService;
 import it.pagopa.pnss.transformation.wsdl.ArubaSignServiceService;
+import lombok.CustomLog;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static it.pagopa.pnss.common.utils.LogUtils.INITIALIZING_ARUBA_PROXY_CLIENT;
+
 
 @Configuration
+@CustomLog
 public class ArubaSignServiceConf {
     @Value("${aruba.server.address}")
     private String arubaServerAddress;
     @Bean
     public ArubaSignService arubaSignService() {
-
+            var endpointName = ArubaSignServiceService.ArubaSignServicePort;
+            var serviceName = ArubaSignServiceService.SERVICE;
+            log.debug(INITIALIZING_ARUBA_PROXY_CLIENT, "pn-ss", arubaServerAddress, endpointName, serviceName);
             JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
             factory.setServiceClass(ArubaSignService.class);
             factory.setAddress(arubaServerAddress);
-            factory.setWsdlLocation(ArubaSignServiceService.WSDL_LOCATION.getPath());
-            factory.setEndpointName(ArubaSignServiceService.ArubaSignServicePort);
-            factory.setServiceName(ArubaSignServiceService.SERVICE);
+            factory.setEndpointName(endpointName);
+            factory.setServiceName(serviceName);
             return factory.create(ArubaSignService.class);
         }
 }

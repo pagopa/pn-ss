f89f68ece536dd9540ccf949aa9e2b695988c313
michelescara <michele.scaramuzzino@gmail.com>
2024-03-05 19:15:08 +0100

    PN-8746: Added byte array to PnSignDocumentResponse in AlternativeSignProviderService.

--- src/main/java/it/pagopa/pn/library/sign/service/impl/AlternativeSignProviderService.java
+++ src/main/java/it/pagopa/pn/library/sign/service/impl/AlternativeSignProviderService.java
@@ -14,21 +14,33 @@ public class AlternativeSignProviderService implements IPnSignService {
     @Override
     public Mono<PnSignDocumentResponse> signPdfDocument(byte[] fileBytes, Boolean timestamping) {
         log.debug(CLIENT_METHOD_INVOCATION, ALT_SIGN_PDF_DOCUMENT, timestamping);
-        return Mono.just(new PnSignDocumentResponse())
+        return Mono.fromSupplier(() -> {
+                    var response = new PnSignDocumentResponse();
+                    response.setSignedDocument(new byte[0]);
+                    return response;
+                })
                 .doOnNext(result -> log.info(CLIENT_METHOD_RETURN, ALT_SIGN_PDF_DOCUMENT, result));
     }
 
     @Override
     public Mono<PnSignDocumentResponse> signXmlDocument(byte[] fileBytes, Boolean timestamping) {
         log.debug(CLIENT_METHOD_INVOCATION, ALT_XML_SIGNATURE, timestamping);
-        return Mono.just(new PnSignDocumentResponse())
+        return Mono.fromSupplier(() -> {
+                    var response = new PnSignDocumentResponse();
+                    response.setSignedDocument(new byte[0]);
+                    return response;
+                })
                 .doOnNext(result -> log.info(CLIENT_METHOD_RETURN, ALT_XML_SIGNATURE, result));
     }
 
     @Override
     public Mono<PnSignDocumentResponse> pkcs7Signature(byte[] fileBytes, Boolean timestamping) {
         log.debug(CLIENT_METHOD_INVOCATION, ALT_PKCS_7_SIGN_V2, timestamping);
-        return Mono.just(new PnSignDocumentResponse())
+        return Mono.fromSupplier(() -> {
+                    var response = new PnSignDocumentResponse();
+                    response.setSignedDocument(new byte[0]);
+                    return response;
+                })
                 .doOnNext(result -> log.info(CLIENT_METHOD_RETURN, ALT_PKCS_7_SIGN_V2, result));
     }
 }

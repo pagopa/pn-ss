064556f731d61e6e2fa2810f9e033b2018b55b99
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-04-05 14:49:30 +0200

    PN-9625-bis: changed retry conditions to prevent timeouts when not necessary

--- src/main/java/it/pagopa/pnss/configuration/RetryConfiguration.java
+++ src/main/java/it/pagopa/pnss/configuration/RetryConfiguration.java
@@ -1,5 +1,8 @@
 package it.pagopa.pnss.configuration;
 
+import it.pagopa.pnss.common.client.exception.DocumentKeyNotPresentException;
+import it.pagopa.pnss.common.client.exception.DocumentTypeNotPresentException;
+import it.pagopa.pnss.common.client.exception.IdClientNotFoundException;
 import it.pagopa.pnss.configurationproperties.DynamoRetryStrategyProperties;
 import it.pagopa.pnss.configurationproperties.GestoreRepositoryRetryStrategyProperties;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +12,11 @@ import org.springframework.context.annotation.Configuration;
 import reactor.util.retry.Retry;
 import reactor.util.retry.RetryBackoffSpec;
 import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.time.Duration;
+import java.util.function.Predicate;
 
 import static it.pagopa.pnss.common.utils.LogUtils.RETRY_ATTEMPT;
 
@@ -24,6 +29,8 @@ public class RetryConfiguration {
     @Autowired
     private GestoreRepositoryRetryStrategyProperties gestoreRepositoryRetryStrategyProperties;
 
+    private final Predicate<Throwable> isNotFound = throwable -> (throwable instanceof DocumentKeyNotPresentException) || (throwable instanceof IdClientNotFoundException)  || (throwable instanceof DocumentTypeNotPresentException);
+
     @Bean
     RetryBackoffSpec dynamoRetryStrategy() {
         return Retry.backoff(dynamoRetryStrategyProperties.maxAttempts(), Duration.ofSeconds(dynamoRetryStrategyProperties.minBackoff()))
@@ -35,6 +42,7 @@ public class RetryConfiguration {
     @Bean
     RetryBackoffSpec gestoreRepositoryRetryStrategy() {
         return Retry.backoff(gestoreRepositoryRetryStrategyProperties.maxAttempts(), Duration.ofSeconds(gestoreRepositoryRetryStrategyProperties.minBackoff()))
+                .filter(Predicate.not(isNotFound))
                 .doBeforeRetry(retrySignal -> log.debug(RETRY_ATTEMPT, retrySignal.totalRetries(), retrySignal.failure(), retrySignal.failure().getMessage()))
                 .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> retrySignal.failure());
     }
@@ -43,6 +51,7 @@ public class RetryConfiguration {
     RetryBackoffSpec s3RetryStrategy() {
         return Retry.backoff(gestoreRepositoryRetryStrategyProperties.maxAttempts(), Duration.ofSeconds(gestoreRepositoryRetryStrategyProperties.minBackoff()))
                 .filter(S3Exception.class::isInstance)
+                .filter(Predicate.not(NoSuchKeyException.class::isInstance))
                 .doBeforeRetry(retrySignal -> log.debug(RETRY_ATTEMPT, retrySignal.totalRetries(), retrySignal.failure(), retrySignal.failure().getMessage()))
                 .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> retrySignal.failure());
     }

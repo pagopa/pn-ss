64dd0df2c0763743df538ad9753b2a28efb93491
ElenaPagnacco <116713610+ElenaPagnacco@users.noreply.github.com>
2024-05-02 10:33:27 +0200

    PN-10883: fixed acknowledge on error to happen only if new message is posted on queue

--- src/main/java/it/pagopa/pnss/transformation/service/TransformationService.java
+++ src/main/java/it/pagopa/pnss/transformation/service/TransformationService.java
@@ -95,12 +95,12 @@ public class TransformationService {
                     return objectTransformation(fileKey, detailObject.getBucketOriginDetail().getName(), newStagingBucketObject.getRetry(), true);
                 })
                 .then()
+                .doOnSuccess( s3ObjectDto -> acknowledgment.acknowledge())
                 .doOnError(throwable -> !(throwable instanceof InvalidStatusTransformationException || throwable instanceof IllegalTransformationException), throwable -> log.error("An error occurred during transformations for document with key '{}' -> {}", fileKeyReference.get(), throwable.getMessage()))
                 .onErrorResume(throwable -> newStagingBucketObject.getRetry() <= MAX_RETRIES, throwable -> {
                     newStagingBucketObject.setRetry(newStagingBucketObject.getRetry() + 1);
-                    return sqsService.send(signQueueName, newStagingBucketObject).then();
-                })
-                .doFinally( signalType -> acknowledgment.acknowledge());
+                    return sqsService.send(signQueueName, newStagingBucketObject).then(Mono.fromRunnable(acknowledgment::acknowledge));
+                });
     }
 
     public Mono<DeleteObjectResponse> objectTransformation(String key, String stagingBucketName, int retry, Boolean marcatura) {

5f4a2f939b34e15c39d3aa1d4c9a3ca2e956207d
michelescara <michele.scaramuzzino@gmail.com>
2024-05-07 16:21:16 +0200

    PN-10927: Fixed statemachine retry strategy property names

--- src/main/java/it/pagopa/pnss/configurationproperties/retry/StateMachineRetryStrategyProperties.java
+++ src/main/java/it/pagopa/pnss/configurationproperties/retry/StateMachineRetryStrategyProperties.java
@@ -2,7 +2,7 @@ package it.pagopa.pnss.configurationproperties.retry;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "sm.retry.strategy")
+@ConfigurationProperties(prefix = "pn.ss.statemachine.retry.strategy")
 public record StateMachineRetryStrategyProperties(Long maxAttempts, Long minBackoff) {
 }
 

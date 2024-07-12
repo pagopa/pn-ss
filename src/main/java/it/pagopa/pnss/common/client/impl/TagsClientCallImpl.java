d56aa98297aa74a0f8e7e5c28f966242e94d5597
ElenaPagnacco <116713610+ElenaPagnacco@users.noreply.github.com>
2024-07-12 15:55:28 +0200

    PN-11941: implemented red phase

--- src/main/java/it/pagopa/pnss/common/client/impl/TagsClientCallImpl.java
+++ src/main/java/it/pagopa/pnss/common/client/impl/TagsClientCallImpl.java
@@ -1,11 +1,14 @@
 package it.pagopa.pnss.common.client.impl;
 
 
+import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.TagsChanges;
+import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.TagsResponse;
 import it.pagopa.pnss.common.client.TagsClientCall;
 import lombok.CustomLog;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @CustomLog
 @Service
@@ -23,5 +26,14 @@ public class TagsClientCallImpl implements TagsClientCall {
         this.ssWebClient = ssWebClient;
     }
 
+    @Override
+    public Mono<TagsResponse> putTags(String documentKey, TagsChanges tagsChanges) {
+        return ssWebClient.put()
+                          .uri( builder -> builder.path(anagraficaTagsClientEndpointPut)
+                          .build(documentKey))
+                          .bodyValue(tagsChanges)
+                          .retrieve()
+                          .bodyToMono(TagsResponse.class);
+    }
 
 }
--- src/main/java/it/pagopa/pnss/indexing/rest/AdditionalFileTagsController.java
+++ src/main/java/it/pagopa/pnss/indexing/rest/AdditionalFileTagsController.java
@@ -1,9 +1,20 @@
 package it.pagopa.pnss.indexing.rest;
 
+import it.pagopa.pn.safestorage.generated.openapi.server.v1.api.AdditionalFileTagsApi;
+import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.AdditionalFileTagsUpdateRequest;
+import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.AdditionalFileTagsUpdateResponse;
 import lombok.CustomLog;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @RestController
 @CustomLog
-public class AdditionalFileTagsController {
+public class AdditionalFileTagsController implements AdditionalFileTagsApi {
+
+    @Override
+    public Mono<ResponseEntity<AdditionalFileTagsUpdateResponse>> additionalFileTagsUpdate(String fileKey, Mono<AdditionalFileTagsUpdateRequest> additionalFileTagsUpdateRequest, ServerWebExchange exchange) {
+        return AdditionalFileTagsApi.super.additionalFileTagsUpdate(fileKey, additionalFileTagsUpdateRequest, exchange);
+    }
 }

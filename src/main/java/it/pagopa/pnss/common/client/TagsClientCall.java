e099e8487ef4fd427333561848c586de4e575746
michelescara <michele.scaramuzzino@gmail.com>
2024-07-17 15:46:58 +0200

    PN-11945: Fixed red phase.

--- src/main/java/it/pagopa/pnss/common/client/TagsClientCall.java
+++ src/main/java/it/pagopa/pnss/common/client/TagsClientCall.java
@@ -1,11 +1,9 @@
 package it.pagopa.pnss.common.client;
 
-
-import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.TagsResponse;
+import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.TagsRelationsResponse;
 import reactor.core.publisher.Mono;
 
 public interface TagsClientCall {
-    Mono<TagsResponse> getTags(String tagKeyValue);
-
+    Mono<TagsRelationsResponse> getTagsRelations(String tagKeyValue);
 
 }

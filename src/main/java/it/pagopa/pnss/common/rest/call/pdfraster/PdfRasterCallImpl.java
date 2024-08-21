94dd9c27d611fabb02d765e773f05b0057a5a8c1
Nico Ponziani <nico.ponziani@dgsspa.com>
2024-08-21 09:50:42 +0200

    feature_PN-12317 fix 804_802 Added retry strategy to convertPdf() call 1

--- src/main/java/it/pagopa/pnss/common/rest/call/pdfraster/PdfRasterCallImpl.java
+++ src/main/java/it/pagopa/pnss/common/rest/call/pdfraster/PdfRasterCallImpl.java
@@ -1,10 +1,17 @@
 package it.pagopa.pnss.common.rest.call.pdfraster;
 
 import it.pagopa.pnss.common.configurationproperties.endpoint.internal.pdfraster.PdfRasterEndpointProperties;
+import it.pagopa.pnss.configurationproperties.PdfRasterRetryStrategyProperties;
 import lombok.CustomLog;
+import lombok.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+import reactor.util.retry.RetryBackoffSpec;
+
+import java.time.Duration;
 
 @CustomLog
 @Component
@@ -12,19 +19,23 @@ public class PdfRasterCallImpl implements PdfRasterCall {
 
     private final PdfRasterEndpointProperties pdfRasterEndpointProperties;
     private final WebClient pdfRasterWebClient;
+    private final RetryBackoffSpec pdfRasterRetryStrategy;
 
-    public PdfRasterCallImpl(PdfRasterEndpointProperties pdfRasterEndpointProperties, WebClient pdfRasterWebClient) {
+    public PdfRasterCallImpl(PdfRasterEndpointProperties pdfRasterEndpointProperties, WebClient pdfRasterWebClient, RetryBackoffSpec pdfRasterRetryStrategy) {
         this.pdfRasterEndpointProperties = pdfRasterEndpointProperties;
         this.pdfRasterWebClient = pdfRasterWebClient;
+        this.pdfRasterRetryStrategy = pdfRasterRetryStrategy;
     }
 
     public Mono<byte[]> convertPdf(byte[] fileBytes) {
         log.logInvokingExternalService("pn-pdfraster", "convertPdf()");
         return pdfRasterWebClient.post()
                 .uri(pdfRasterEndpointProperties.convertPdf())
+                .contentType(MediaType.MULTIPART_FORM_DATA)
                 .bodyValue(fileBytes)
                 .retrieve()
-                .bodyToMono(byte[].class);
+                .bodyToMono(byte[].class)
+                .retryWhen(pdfRasterRetryStrategy);
     }
 
 }

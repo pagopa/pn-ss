c8d683f7404c41ba407da66ed238c31e684a513b
NicoPonziani <84712226+NicoPonziani@users.noreply.github.com>
2024-09-10 15:30:19 +0200

    Feature/pn 12317 (#627)
    * [796_798] Green Phase
    
    * [796_799] Blue Phase
    
    * [796_799] fix missing semaphore release
    
    * feature_PN-12317 fix 804_802 Added retry strategy to convertPdf() call 1
    
    * fix body convertPdf call
    
    * fix filename in pdfraster call
    
    * [805] fix log dump pdf
    
    * PN-12317: added codec buffer size parameter (#628)
    
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    
    * PN-12317: added codec buffer size parameter in class WebClientConf (#630)
    
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    
    * PN-12317: fixed SpringCodecMaxInMemorySize value to integer (#631)
    
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    Co-authored-by: mottone-dgs <119047198+mottone-dgs@users.noreply.github.com>
    
    * PN-12317: fixed SecurityConfiguration
    
    * PN-12317: Added ServiceEndpointToPdfRaster parameter
    
    * PN-12317: fixed wrong base path in
    internal-endpoint.pdf-raster.base-path
    
    ---------
    
    Co-authored-by: Nico Ponziani <nico.ponziani@dgsspa.com>
    Co-authored-by: DamianoLozzi <98752415+DamianoLozzi@users.noreply.github.com>
    Co-authored-by: mottone-dgs <mario.ottone@dgsspa.com>
    Co-authored-by: mottone-dgs <119047198+mottone-dgs@users.noreply.github.com>

--- src/main/java/it/pagopa/pnss/common/utils/LogUtils.java
+++ src/main/java/it/pagopa/pnss/common/utils/LogUtils.java
@@ -96,6 +96,8 @@ public class LogUtils {
     public static final String NEW_STAGING_BUCKET_OBJECT_CREATED_LISTENER = "newStagingBucketObjectCreatedListener()";
     public static final String NEW_STAGING_BUCKET_OBJECT_CREATED = "TransformationService.newStagingBucketObjectCreated()";
     public static final String OBJECT_TRANSFORMATION = "TransformationService.objectTransformation()";
+    public static final String SIGN_AND_TIMEMARK_TRANSFORMATION = "TransformationService.signAndTimemarkTransformation()";
+    public static final String RASTER_TRANSFORMATION = "TransformationService.rasterTransformation()";
     public static final String CHANGE_FROM_STAGING_BUCKET_TO_HOT_BUCKET = "TransformationService.changeFromStagingBucketToHotBucket()";
 
     //RETENTION SERVICE

9b8ffce8922bc76a9a21740df4562e46823e0d92
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-10-15 14:51:17 +0200

    PN-12870: added SchedulingConfiguration to manage scheduled tasks and related properties. Removed annotation where not required

--- src/main/java/it/pagopa/pnss/configuration/IgnoredUpdateMetadataConfig.java
+++ src/main/java/it/pagopa/pnss/configuration/IgnoredUpdateMetadataConfig.java
@@ -7,7 +7,6 @@ import lombok.CustomLog;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.*;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -33,7 +32,6 @@ import static it.pagopa.pnss.common.utils.LogUtils.*;
  * The class handles a scheduled task to periodically update this list.
  */
 @CustomLog
-@EnableScheduling
 @Configuration
 @ConditionalOnExpression("T(org.apache.commons.lang3.StringUtils).isNotEmpty('${pn.ss.ignored.update.metadata.list}')")
 public class IgnoredUpdateMetadataConfig {

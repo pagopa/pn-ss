590de969a38002321f13133b090a7bd81939d840
damiano.lozzi <damiano.lozzi@dgsspa.com>
2024-12-13 14:27:06 +0100

    PN-13370: added missing properties, modified start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+
+
 curr_dir=$(pwd)
 echo "### Starting pn-ss ###"
 
-if ! ../src/test/resources/testcontainers/init.sh; then
+cd ..
+if ! src/test/resources/testcontainers/init.sh; then
   echo "### Failed to run init.sh ###"
   exit 1
 fi
 
-cd ..
 
-if ! mvn clean install spring-boot:run; then
-  echo "### Initialization failed ###"
-  exit 1
+if ! ( ./mvnw -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=local -Daws.accessKeyId=TEST -Daws.secretAccessKey=TEST -Daws.region=eu-south-1" spring-boot:run ); then
+echo "### Initialization failed ###"
+exit 1
 fi
 
 # Return to the original directory

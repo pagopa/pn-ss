ad13970f8e512c083e20a1d1756ed98ad1ad8af4
michelescara <michele.scaramuzzino@gmail.com>
2025-01-31 13:06:58 +0100

    PN-13829: Fixed tests.

--- src/test/java/it/pagopa/pnss/configuration/TransformationConfigTest.java
+++ src/test/java/it/pagopa/pnss/configuration/TransformationConfigTest.java
@@ -2,6 +2,7 @@ package it.pagopa.pnss.configuration;
 
 import it.pagopa.pn.safestorage.generated.openapi.server.v1.dto.DocumentType;
 import it.pagopa.pnss.testutils.annotation.SpringBootTestWebEnv;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +15,17 @@ class TransformationConfigTest {
     private TransformationConfig transformationConfig;
 
     @ParameterizedTest
-    @EnumSource(DocumentType.TransformationsEnum.class)
+    @EnumSource(value = DocumentType.TransformationsEnum.class, names = {"NONE"}, mode = EnumSource.Mode.EXCLUDE)
     void getTransformationQueueName_Ok(DocumentType.TransformationsEnum transformation) {
         String queueName = transformationConfig.getTransformationQueueName(transformation);
         assertNotNull(queueName);
         assertFalse(queueName.isEmpty());
+    }
+
+    @Test
+    void getTransformationQueueName_None() {
+        String queueName = transformationConfig.getTransformationQueueName(DocumentType.TransformationsEnum.NONE);
+        assertNull(queueName);
     }
 
 }

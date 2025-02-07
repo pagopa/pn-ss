ab3ea733c0a8752c07bf780bd2df60484c1ba3ed
michelescara <michele.scaramuzzino@gmail.com>
2025-02-07 12:27:08 +0100

    PN-13832: Removed Pdf Raster API related implementations.

--- src/main/java/it/pagopa/pnss/common/rest/call/pdfraster/PdfRasterCall.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.pnss.common.rest.call.pdfraster;
-
-import reactor.core.publisher.Mono;
-
-public interface PdfRasterCall {
-
-    Mono<byte[]> convertPdf(byte[] fileBytes, String fileKey);
-
-}

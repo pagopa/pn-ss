9e7c65cae58d8678fad9f54cdc23699203b5be02
michelescara <79908782+michelescara@users.noreply.github.com>
2025-02-11 14:28:03 +0100

    Feature/pn 13832 (#697)
    * PN-13832: Removed Pdf Raster API related implementations.
    
    * PN-13832: Removed pdf-raster variables and configurations.

--- src/main/java/it/pagopa/pnss/common/configurationproperties/endpoint/internal/pdfraster/PdfRasterEndpointProperties.java
+++
@@ -1,7 +0,0 @@
-package it.pagopa.pnss.common.configurationproperties.endpoint.internal.pdfraster;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "internal-endpoint.pdf-raster")
-public record PdfRasterEndpointProperties(String containerBaseUrl, String convertPdf) {
-}

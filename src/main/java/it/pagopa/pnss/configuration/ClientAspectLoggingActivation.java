16691c2ebe252e980ae12bc8caf348614883fc00
Riccardo Pompili <riccardo.pompili@dgsspa.com>
2026-09-15 14:33:06 +0200

    PN-21351 - Attivazione degli aspect di logging di piattaforma

---
+++ src/main/java/it/pagopa/pnss/configuration/ClientAspectLoggingActivation.java
@@ -0,0 +1,8 @@
+package it.pagopa.pnss.configuration;
+
+import it.pagopa.pn.commons.utils.ClientAspectLogging;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientAspectLoggingActivation extends ClientAspectLogging {
+}
